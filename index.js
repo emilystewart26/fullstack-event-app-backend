58c35ea39210301ed059925072c3f9d349470e41
JDMcauley <123961794+JDMcauley@users.noreply.github.com>
2025-05-29 10:14:05 +0100

    implement user register

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const PORT = process.env.PORT || 3001;
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
 }));
 app.use(express.json());
